0c64dc6ab5e56a47d6b44a1f33adf0c4ebaa517d
ashish7079 <ashishjha25032004@gmail.com>
2026-09-20 00:26:50 +0530

    Prepare chatbot for deployments

--- chatbot/AICareerChatbot.py
+++ chatbot/AICareerChatbot.py
@@ -418,6 +418,6 @@ if __name__ == "__main__":
 
     app.run(
         host="0.0.0.0",
-        port=5003,
-        debug=True
+        port=int(os.environ.get("PORT", 5003)),
+        debug=False
     )
